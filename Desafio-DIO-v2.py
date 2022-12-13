fc682c2612dee57dea083cb4b77755120b7409d0
Márcio Alexandre Dias <102327035+MarcioXandir@users.noreply.github.com>
2022-12-13 20:08:25 -0200

    13/12
    em produção.

--- Desafio-DIO-v2.py
+++ Desafio-DIO-v2.py
@@ -10,43 +10,39 @@ menu = """
 """
 
 #separar em funcões: sacar, depositar,estrato
-
-
-
 saldo = 0
-saques_realizados = []
-depositos_realizados = []
-qtd_Saques = 3
-limitevalor = 500
 
-def saque(limitevalor):
-    global qtd_Saques,saques_realizados,saldo
-    if qtd_Saques == 0:
-        print("Seu Limite diário de 3 saques foi excedido")
-        input('Pressione ENTER para prosseguir.')
-    else:
-        # caso não haja saldo, msg de falta de saldo
-        valor_saque_unitario = float(input('Valor para saque: '))
-        if valor_saque_unitario > limitevalor or valor_saque_unitario > saldo or valor_saque_unitario <= 0:
-            print(f'Valor Inválido. Saque deve ser Máximo de R$ 500,00 e Mínimo R$ 1,00. Seu saldo atual: R$ {saldo}')
-            input('Pressione ENTER para continuar')
-        else:
-                # armazenar saques em variavel e pode ser exibido em extrato
-            qtd_Saques -= 1
-            saldo -= valor_saque_unitario
-            saques_realizados.append(valor_saque_unitario)
-            print('Seu Saque foi liberado!')
-            input('Pressione ENTER para prosseguir.')
-    
+qtd_saques = 3
+limite_valor = 500
 
+saques_realizados = [] #extrato
+depositos_realizados = []#depositos q foram feitos
 
-def deposito(valor):
-    global saldo, depositos_realizados
+cc = []
+clientes = []
+
+def deposito(saldo, valor, depositos_realizados):
     saldo += valor
     depositos_realizados.append(valor)
+
     print(f'Valor deposito REALIZADO. Seu saldo atual é: R${saldo:7.2f}')
     input('Pressione ENTER para continuar')
+    return saldo, depositos_realizados
 
+def saque(saldo,valor,saques_realizados,limite_valor):
+    global qtd_saques#estou colocando essa aqui como global, daí não irei coloca-la como argumento
+    if valor > limite_valor or valor > saldo or valor <= 0:
+        print(f'Valor Inválido. Saque deve ser Máximo de R$ 500,00 e Mínimo R$ 1,00. Seu saldo atual: R$ {saldo}')
+        input('Pressione ENTER para continuar')
+    else:
+                # armazenar saques em variavel e pode ser exibido em extrato
+        qtd_saques -= 1
+        saldo -= valor
+        saques_realizados.append(valor)
+        print('Seu Saque foi liberado!')
+        input('Pressione ENTER para prosseguir.')
+
+    return saldo, saques_realizados
 
 def extrato(saldo):
     print('\n\n..........INICIO EXTRATO.........')
@@ -64,6 +60,13 @@ def extrato(saldo):
 
     input('\n\nPressione ENTER para prosseguir.')
 
+def cad_cliente(isnumber):
+    cli_cpf = input('Informe o cpf do cliente, somente numeros: ')
+    cli_nome = input('Informe o nome cliente: ')
+    cli_endereco = input('Informe o endereço do cliente: ')
+    cli_dt_nascimento = input('informe a data de nascimento: ')
+
+    return cli_cpf, cli_nome, cli_dt_nascimento, cli_endereco
 
 
 while True:
@@ -82,20 +85,22 @@ while True:
 
         else:
             # deverão ser armazenados em variavel
-            deposito(valor) #,saldo,depositos_realizados)
-
+            saldo, depositos_realizados = deposito(saldo, valor, depositos_realizados) #,saldo,depositos_realizados)
 
     elif opcao == 'E':
         #listar todos depositos e saques realizados na conta.
 
-        extrato(saldo=saldo)
-
-
+        extrato(saldo)
 
     elif opcao == 'S':
-        #limite de 3 saques diarios e 500 por saque
-        saque(limitevalor)
+        if qtd_saques == 0:
+            print("Seu Limite diário de 3 saques foi excedido")
+            #input('Pressione ENTER para prosseguir.')
+        else:
+            # caso não haja saldo, msg de falta de saldo
+            valor = float(input('Informe o valor de saque: '))
+            saldo, saques_realizados = saque(saldo=saldo,valor=valor,saques_realizados=saques_realizados,limite_valor=limite_valor)
 
-         
+
     else:
         print('Opção invalida!')
--- tests.py
+++ tests.py
@@ -1,14 +1,25 @@
-def deposito(valor, saldo,extrato):
-    saldo += valor
-    extrato = valor
-    return saldo, extrato
+saldo = 1000
+qtd_saques = 3
+limite_valor = 500
+saques_realizados = [] #extrato
+#depositos_realizados = []#depositos q foram feitos
+#cc = []
+#clientes = []
 
+def saque(saldo,valor,saques_realizados,qtd_saques,limite_valor):
+    if valor > limite_valor or valor > saldo or valor <= 0:
+        print(f'Valor Inválido. Saque deve ser Máximo de R$ 500,00 e Mínimo R$ 1,00. Seu saldo atual: R$ {saldo}')
+        input('Pressione ENTER para continuar')
+    else:
+                # armazenar saques em variavel e pode ser exibido em extrato
+        qtd_saques -= 1
+        saldo -= valor
+        saques_realizados.append(valor)
+        print('Seu Saque foi liberado!')
+        input('Pressione ENTER para prosseguir.')
 
+    return saldo, saques_realizados
 
-v = 1000
-s = 0
-ext = 0
+valor = float(input('Informe o valor de saque: '))
+saldo, saques_realizados = saque(saldo=saldo,valor=valor,saques_realizados=saques_realizados,qtd_saques=qtd_saques,limite_valor=limite_valor)
 
-saldo, extrato = deposito(v,s,ext)
-
-print(saldo, extrato)
