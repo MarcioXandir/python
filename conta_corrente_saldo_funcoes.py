01340a2c54737f71f66ee74b934bfc8ad212729d
Márcio Alexandre Dias <102327035+MarcioXandir@users.noreply.github.com>
2022-12-15 16:18:26 -0200

    a
    a

--- conta_corrente_saldo_funcoes.py
+++ conta_corrente_saldo_funcoes.py
@@ -4,10 +4,10 @@ menu = """
 (S) SAQUE
 (E) EXTRATO
 (D) DEPOSITO
-(N) CADASTRA CLIENTE
-(C) CADASTRA C.C.
-(T) CONSULTA C.C.
-(Q) SAIR 
+(C) CADASTRAR CLIENTE
+(G) GERAR C.C.
+(M) MOSTRAR C.C.
+(Q) SAIR
 ..........................
 ##########################
 """
@@ -24,26 +24,30 @@ depositos_realizados = []#depositos q foram feitos
 cc = []
 cadastro_clientes = []
 
+def msg_salta_linha():
+    print('\n>>>>>>>>>>>>>>>>>>>>>>>>\nPRESSIONE ENTER PARA CONTINUAR!')
+    input()
+
 def deposito(saldo, valor, depositos_realizados):
     saldo += valor
     depositos_realizados.append(valor)
 
     print(f'Valor deposito REALIZADO. Seu saldo atual é: R${saldo:7.2f}')
-    input('Pressione ENTER para continuar')
+    msg_salta_linha()
     return saldo, depositos_realizados
 
 def saque(saldo,valor,saques_realizados,limite_valor):
     global qtd_saques#estou colocando essa aqui como global, daí não irei coloca-la como argumento
     if valor > limite_valor or valor > saldo or valor <= 0:
         print(f'Valor Inválido. Saque deve ser Máximo de R$ 500,00 e Mínimo R$ 1,00. Seu saldo atual: R$ {saldo}')
-        input('Pressione ENTER para continuar')
+        msg_salta_linha()
     else:
                 # armazenar saques em variavel e pode ser exibido em extrato
         qtd_saques -= 1
         saldo -= valor
         saques_realizados.append(valor)
         print('Seu Saque foi liberado!')
-        input('Pressione ENTER para prosseguir.')
+        msg_salta_linha()
 
     return saldo, saques_realizados
 
@@ -61,35 +65,42 @@ def extrato(saldo):
     print(f'\nSeu saldo atual é: {saldo:10.2f}')
     print('...........FIM Extrato...........')
 
-    input('\n\nPressione ENTER para prosseguir.')
+    msg_salta_linha()
 
-def cad_cliente():
-    cli_cpf = int(input('Informe o cpf do cliente, somente numeros: '))
+def cadastrar_cliente(cli_cpf):
     cli_nome = input('Informe o nome cliente: ')
     cli_endereco = input('Informe o endereço do cliente: ')
-    cli_dt_nascimento = input('informe a data de nascimento: ')
-
-    return cli_cpf, cli_nome, cli_dt_nascimento, cli_endereco
-
-def cadastro_cc(cadastro_clientes,cc):
+    cli_dt_nascimento = input('informe a data de nascimento: ')        
+    return cli_cpf, cli_nome, cli_dt_nascimento, cli_endereco             
+            
+def gerar_conta_corrente(conta):
+    correntista = conta
     agencia = '0001'
     numero_cc = len(cc)+1
-    cpf_correntista = cadastro_clientes[0]
-    nome_correntista = cadastro_clientes[1]
-    print('conta corrente criada!')
+    nova_conta = dict(correntista=correntista,agencia=agencia,numero_cc=numero_cc)
 
-    return numero_cc,agencia,nome_correntista,cpf_correntista
 
+    return nova_conta
+          
 def mostra_cc(cc):
     for i in range(len(cc)):
-        print(f'CPF: {cc[i][3]}')
-        print(f'CC: {cc[i][0]}')
-        print(f'Agência: {cc[i][1]}')
-        print(f'Correntista: {cc[i][2]}')
-
-
-
-
+        
+        print(f'CPF: {cc[i]["correntista"]}')
+        print(f'Nome: {cadastro_clientes[i][1]}')
+        print(f'Data Nascimento: {cadastro_clientes[i][2]}')
+        print(f'Endereço: {cadastro_clientes[i][3]}')
+        aux = cc[i]['correntista']
+        #print(cadastro_clientes)
+        #print(type(cadastro_clientes))
+        #print(len(cadastro_clientes))
+        
+        print(20*'.')
+        print(f"CC: {cc[i]['agencia']}")
+        print(f"Agência: {cc[i]['numero_cc']}")
+        #for i2 in range(len(cc)):
+        #    if cc[]['correntista'] == aux:
+        print(20*'-')
+#        msg_salta_linha()
 
 while True:
     print(menu)
@@ -98,23 +109,46 @@ while True:
         print("Obrigado por usar nossos serviços!!")
         break
     
-    elif opcao == "N":
-        cadastro_clientes = cad_cliente()
-        dados_cc = cadastro_cc(cadastro_clientes,cc)
-        cc.append(dados_cc)
-
     elif opcao == "C":
-        cadastro_cc(cc=cc,cadastro_clientes=cadastro_clientes)
+        cli_cpf = input('Informe o cpf do cliente, somente numeros: ')
 
-    elif opcao == 'T':
+        for i in range(len(cadastro_clientes)):
+            #print(f'todo cadastro cliente: {cadastro_clientes}') # 1 indice com 4 registros
+            #print(f'cadastro_clientes indice [i]: {cadastro_clientes[i]}')#1 indice com 4 registros
+            #print(f'cadastro_clientes indice [i][0]: {cadastro_clientes[i][0]}')# mostra o indice 0 do 1 registro>> : 11
+            #input('enter')
+            if cli_cpf == cadastro_clientes[i][0]:
+                print(f'CPF JÁ CADASTRADO PARA O CLIENTE DE NOME: {cadastro_clientes[i][1]}')
+                input()
+                break
+        else:
+            dados = (cadastrar_cliente(cli_cpf))
+            cadastro_clientes.append(dados)
+    
+    elif opcao == 'M':
         mostra_cc(cc)
+
+    elif opcao == 'G':
+        cliente_nova_cc = input('Informe CPF cliente para nova CC: ')
+
+        for i in range(len(cadastro_clientes)):
+            if cliente_nova_cc == cadastro_clientes[i][0]:
+                dadoscc = gerar_conta_corrente(conta=cliente_nova_cc) #entrei com cpf digitado paralá dentro criar o dict e retorna-lo feito
+                cc.append(dict(dadoscc))
+                print('\nCONTA CRIADA!')
+                print(f"\n\nCorrentista: {dadoscc['correntista']} \nAgencia: {dadoscc['agencia']} \nConta Corrente nº:{dadoscc['numero_cc']},")
+                msg_salta_linha()
+                break
+        else:
+            print('cliente precisa ser cadastrado primeiro.')
+                
     
     elif opcao == 'D':
         valor = float(input("Informe o valor de deposito: "))
         # depositos somente positivos,
         if valor < 0:
             print('valor negativo é invalido para depósito.')
-            input('PRESSIONE ENTER PRA CONTINUAR')
+            msg_salta_linha()
 
         else:
             # deverão ser armazenados em variavel
@@ -128,12 +162,13 @@ while True:
     elif opcao == 'S':
         if qtd_saques == 0:
             print("Seu Limite diário de 3 saques foi excedido")
+            msg_salta_linha()
+
             #input('Pressione ENTER para prosseguir.')
         else:
             # caso não haja saldo, msg de falta de saldo
             valor = float(input('Informe o valor de saque: '))
             saldo, saques_realizados = saque(saldo=saldo,valor=valor,saques_realizados=saques_realizados,limite_valor=limite_valor)
-
 
     else:
         print('Opção invalida!')
--- tests.py
+++ tests.py
@@ -1,30 +1,3 @@
-'''import pyautogui as pg
-p=pg.position() #mosta posição do mouse
-print(p)
-'''
-import time
+lista = ['coco',{'nome': 'marcio','idade':44},12]
 
-class Bicicletas():
-    def __init__(self,marca,aro=18) -> None:
-        self.marca = marca
-        self.aro = aro
-
-
-    def pedala(self):
-  
-        for i in 'PEDALANDO':
-            print(f'{i} ',end='')
-            
-           
-
-    def parar(self):
-        print('Parando de pedalar.')
-        print("Parou!!")
-
-cros = Bicicletas('caloi')
-
-cros.pedala()
-time.sleep = 5
-cros.parar()
-time.sleep = 5
-print(cros.marca)
+print(lista[1]['nome'])
